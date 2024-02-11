20d95966726d254a4229b8c709d7a0d60149aed6
YuliyaYuliya <yr555556666667777777@gmail.com>
2024-02-11 20:31:16 +0300

    build.gradle ch src ch

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,13 +1,23 @@
 package ru.netology.web;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class CallbackTest {
 
     private WebDriver driver;
+
+    @BeforeAll
+
+    static  void  setUpAll() {
+        System.setProperty("webDriver.chrome.driver", "driver/win/chromedriver.exe");
+    }
 
     @BeforeEach
     void setUp() { driver = new ChromeDriver(); }
@@ -22,6 +32,8 @@ public class CallbackTest {
     @Test
 
     void shouldTestSomething() {
-        throw new UnsupportedOperationException();
+        //загрузить страницу
+        //поиск элементов
+        //взаимодействие с элементами
     }
 }
